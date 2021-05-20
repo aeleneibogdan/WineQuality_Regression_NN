2708d9474928380ee36d1e1fffe3fa09d12ab878
Aelenei Bogdan-Stefan <65165872+aeleneibogdan@users.noreply.github.com>
2021-05-21 02:51:10 +0300

    Update NNHW2_MLPRegressor.py

--- NNHW2_MLPRegressor.py
+++ NNHW2_MLPRegressor.py
@@ -17,12 +17,10 @@ from sklearn.metrics import mean_squared_error
 from sklearn.metrics import r2_score
 from sklearn.preprocessing import StandardScaler
 
+
 datasetwhite=pd.read_csv('winequality-white.csv',sep=';',header=0)
 
 datasetred=pd.read_csv('winequality-red.csv',sep=';',header=0)
-
-sb.pairplot(datasetwhite, hue='quality',height=1.5,palette="PuBu")
-sb.pairplot(datasetred, hue='quality',height=1.5)
 
 
 #splitting the last column from the entire white wine dataset
@@ -33,18 +31,22 @@ tWhite=datasetwhite['quality']
 xRed=datasetred.iloc[:,:11]
 tRed=datasetred['quality']
 
+# sb.pairplot(datasetwhite, hue='quality',height=1.5,palette="PuBu")
+# sb.pairplot(datasetred, hue='quality',height=1.5)
 
 #splitting the white wine dataset
 xTrainWhite, xTestWhite, tTrainWhite, tTestWhite = train_test_split(xWhite, tWhite, test_size=0.2)
 
+#scaling the white wine dataset
 scaler = StandardScaler()
 scaler.fit(xTrainWhite)
 xTrainWhite = scaler.transform(xTrainWhite)
 xTestWhite = scaler.transform(xTestWhite)
 
-#splitting the white wine dataset
-xTrainRed, xTestRed, tTrainRed, tTestRed = train_test_split(xRed, tRed, test_size=0.6)
+#splitting the red wine dataset
+xTrainRed, xTestRed, tTrainRed, tTestRed = train_test_split(xRed, tRed, test_size=0.3)
 
+#scaling the red wine dataset
 scaler.fit(xTrainRed)
 xTrainRed = scaler.transform(xTrainRed)
 xTestRed = scaler.transform(xTestRed)
@@ -54,9 +56,9 @@ mlp = MLPRegressor(activation="relu", solver="adam", hidden_layer_sizes=(5,5),ma
 #Training the White wine data
 mlp.fit(xTrainWhite, tTrainWhite)
 
+
 #Training the Red wine data
 mlp.fit(xTrainRed, tTrainRed)
-
 
 yTrainWhite = mlp.predict(xTrainWhite)
 yTestWhite = mlp.predict(xTestWhite)
@@ -73,21 +75,16 @@ mseTestWhite = mean_squared_error(tTestWhite, yTestWhite)
 rmseTestWhite = mean_squared_error(tTestWhite, yTestWhite, squared=False)
 rmseTrainWhite = mean_squared_error(tTrainWhite, yTrainWhite, squared=False)
 
-
+rsquaredscoreWhite=r2_score(tTestWhite, yTestWhite)
 
 print('\n')
-
-# tTrainWhite.reshape(-1,1)
-# whiteTrainAccuracy=mlp.score(tTrainWhite, yTrainWhite)
-# whiteTestAccuracy=mlp.score(tTestWhite, yTestWhite)
-# print("Train accuracy for the White wine dataset", whiteTrainAccuracy)
-# print("Test accuracy for the White wine dataset", whiteTestAccuracy)
 
 print("Mean absolute error for TEST formula is:", maeTestWhite)
 print("Mean absolute error for TRAIN formula is:", maeTrainWhite)
 print("Mean squared error for TEST formula is:", mseTestWhite)
 print("RMSE for TEST formula", rmseTestWhite)
 print("RMSE for TRAIN formula", rmseTrainWhite)
+print("R2 score for WHITE wine is: ", rsquaredscoreWhite)
 
 print('===================================================================')
 
@@ -101,19 +98,16 @@ mseTestRed = mean_squared_error(tTestRed, yTestRed)
 rmseTestRed = mean_squared_error(tTestRed, yTestRed, squared=False)
 rmseTrainRed = mean_squared_error(tTrainRed, yTrainRed, squared=False)
 
-# tTrainRed.reshape(-1,1)
-# redTrainAccuracy=mlp.score(tTrainRed, yTrainRed)
-# redTestAccuracy=mlp.score(tTestRed, yTestRed)
-# print("Train accuracy for the Red wine dataset", redtrainaccuracy)
-# print("Test accuracy for the Red wine dataset", redTestAccuracy)
+rsquaredscoreRed=r2_score(tTestRed, yTestRed)
 
 print("Mean absolute error for TEST formula is:", maeTestRed)
 print("Mean absolute error for TRAIN formula is:", maeTrainRed)
 print("Mean squared error for TEST formula is:", mseTestRed)
 print("RMSE for TEST formula", rmseTestRed)
 print("RMSE for TRAIN formula", rmseTrainRed)
+print("R2 score for RED wine is: ", rsquaredscoreRed)
 
-
+#Plot loss function
 plt.figure()
 loss_values = mlp.loss_curve_
 plt.plot(loss_values)
